0147d3e405124f0c13fcfb0db0a0bb98d007a2f8
Patricia P <andrograf31@gmail.com>
2023-07-04 19:55:17 +0200

    feat: create room page

---
+++ pages/room.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Room One</h1>
+        <h3>Chairs</h3>
+        <p>chair1</p>
+        <p>status: </p>
+        <button>Reserve</button>
+        <p>chair2</p>
+        <p>status: </p>
+        <button>Reserve</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
