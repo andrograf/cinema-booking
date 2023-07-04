ec911bcb9b56dc9d53b1b099ec051b0dccfe6d14
Patricia P <andrograf31@gmail.com>
2023-07-04 20:28:12 +0200

    feat: add routes to pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <NuxtLink to="/login">login</NuxtLink>
+        <br>
+        <NuxtLink to="/signup">signup</NuxtLink>
       <h1>Cinema - Home</h1>
       <NuxtLink to="/room">Book seats</NuxtLink>
     </div>
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <NuxtLink to="/">Return home</NuxtLink>
         <h1>Log in</h1>
         <loginForm/>
     </div>
