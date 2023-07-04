8cc5923d37d0d450a26eeaaa7c1f5cea6c29e376
Patricia P <andrograf31@gmail.com>
2023-07-04 19:50:39 +0200

    feat: create index page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Cinema - Home</h1>
+      <p>lorem ipsum</p>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+  p {
+    margin-top: 20px;
+  }
+  </style>
+  
