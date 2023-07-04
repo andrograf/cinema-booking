810a75dcb44b54439ed64390cb923309e404a1f6
Patricia P <andrograf31@gmail.com>
2023-07-04 20:17:09 +0200

    feat: create signup/login page

---
+++ pages/registerLogin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Sign Up/Log in</h1>
+        <label for="id">Email</label>
+        <input id="id" type="email"/>
+        <label for="password">Email</label>
+        <input id="password" type="password"/>
+        <button type="submit">Send</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
