2fbef3db89b8b8025abc9d46f7d32e0d06c98e73
Patricia P <andrograf31@gmail.com>
2023-07-04 21:03:54 +0200

    feat: edit login form, add frontend validation

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <label for="id">Email</label>
-        <input id="id" type="email"/>
-        <label for="password">Password</label>
-        <input id="password" type="password"/>
-        <button type="submit">Send</button>
+        <form action="post">
+            <label for="id">Email</label>
+            <input id="id" type="email" required/>
+            <label for="password">Password</label>
+            <input id="password" type="password" minlength="4" required/>
+            <button type="submit">Send</button>
+        </form>
     </div>
 </template>
