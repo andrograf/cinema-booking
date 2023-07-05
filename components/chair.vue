df85384024d8c77762f6b9b571e0b1775c54c50f
Patricia P <andrograf31@gmail.com>
2023-07-05 09:41:54 +0200

    feat: add logic to chair status

--- components/chair.vue
+++ components/chair.vue
@@ -1,7 +1,31 @@
 <template>
     <div>
         <p>chair</p>
-        <p>status: </p>
-        <button>Reserve</button>
+        <p>status: {{status}}</p>
+        <button onclick="increase()">Reserve</button>
     </div>
   </template>
+<script setup>
+let status;
+let count = 0;
+function increase(){
+    count++;
+    alert(count);
+}
+function reset(){
+    count = 0;
+}
+switch(count){
+    case 1:
+        status = "reserved";
+        setTimeout(reset, 120000);
+        break;
+    case 2:
+        status = "sold";
+        break;
+    default:
+        status = "free";
+        break;
+        
+}
+</script>
