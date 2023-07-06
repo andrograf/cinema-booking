86f4c30c4d05204c639bf4e8108cac3dd4bde8c7
Patricia P <andrograf31@gmail.com>
2023-07-06 21:38:41 +0200

    feat: add default records to tables

--- script.ts
+++ script.ts
@@ -1,9 +1,28 @@
-import { PrismaClient } from '@prisma/client'
-
+const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+  // Prisma Client queries
+  const room = await prisma.room.create({
+    data: {
+      name: 'Room One',
+    },
+  })
+  const chairOne = await prisma.chair.create({
+    data: {
+      name: '1.sor 1.szek',
+      roomId: 1
+    },
+  })
+  const chairTwo = await prisma.chair.create({
+    data: {
+      name: '1.sor 2.szek',
+      roomId: 1
+    },
+  })
+  console.log(room)
+  console.log(chairOne)
+  console.log(chairTwo)
 }
 
 main()
