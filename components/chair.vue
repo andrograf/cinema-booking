6ac300fdc09319d7f6a4e6deab38025b582b1c0c
Patricia P <andrograf31@gmail.com>
2023-07-07 14:31:20 +0200

    feat: add logic for update records

--- components/chair.vue
+++ components/chair.vue
@@ -5,13 +5,13 @@
                 <p>{{s.name}}</p>
                 <p>status: {{s.status}}</p>
                 <div v-if="s.status == 'szabad'">
-                    <button >Add to cart</button>
+                    <button onclick="changeStatus()">Add to cart</button>
                 </div>
                 <div v-if="s.status == 'foglalt'">
                     <button v-bind:disabled="true" title="Check back later!">Not available</button>
                 </div>
                 <div v-if="s.status == 'eladva'">
-                    <button v-bind:disabled="true">sold out</button>
+                    <button v-bind:disabled="true">{{s.status}}</button>
                 </div>
             </div>
         </div>
@@ -21,6 +21,25 @@
 
 
 <script setup>
+import { clearInterval } from 'timers';
+
  const {data: rooms} = await useFetch('/api/room');
  const {data: seats} = await useFetch('/api/seat');
+
+ function changeStatus() {
+     console.log("foglalt");
+     let date = Date.now();
+     let newDate = addMinutes(date, 2);
+     let x = setInterval(function(){
+         let distance = newDate - date;
+         if (distance < 0){
+             clearInterval(x);
+             console.log("szabad");
+        }
+    })
+ }
+ function addMinutes(date, minutes){
+    date.setMinutes(date.getMinutes + minutes);
+    return date
+ }
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   pages: true,
   serverHandlers: [
-    { route: '/api/seat', handler: '~/server/api/seat.get.ts' }
+    { route: '/api/seat', handler: '~/server/api/seat.get.ts' },
+    { route: '/api/seatPUT', handler: '~/server/api/seat.put.ts' }
   ]  
 })
